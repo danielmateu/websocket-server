34acd105225ca0b820f91dd4fc01e6a328909c1a
Daniel Mateu <danielmateu86@gmail.com>
2022-11-18 17:58:42 +0100

    Mensajes de conexión y desconexion entre cliente y servidor

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,21 @@
 
+//Referencias HTML
+const lblOnline =  document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline') 
+
 
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+})
+
+socket.on('disconnect', () => {
+    console.log('Desonectado del servidor');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+})
